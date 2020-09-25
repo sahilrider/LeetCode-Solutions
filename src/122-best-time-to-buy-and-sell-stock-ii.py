43f9fb8f839f4298b1bfccf9debf3b3c2d86c9f2
sahilrider <sahilriders@gmail.com>
2020-09-25 23:28:27 +0530

    Revision Day1 25/9/20

--- src/122-best-time-to-buy-and-sell-stock-ii.py
+++ src/122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,17 +1,29 @@
 '''https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/'''
 
+# class Solution:
+#     def maxProfit(self, prices: List[int]) -> int:
+#         if not prices:
+#             return 0
+#         profit = 0 
+#         cp = prices[0]
+#         for i in prices[1:]:
+#             # print(cp)
+#             if i<cp:
+#                 cp = i
+#             elif i>cp:
+#                 profit += i-cp
+#                 cp = i
+#         return profit
+
+#second attempt     
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         if not prices:
             return 0
         profit = 0 
         cp = prices[0]
-        for i in prices[1:]:
-            # print(cp)
-            if i<cp:
-                cp = i
-            elif i>cp:
-                profit += i-cp
-                cp = i
+        for i in range(1, len(prices)):
+            if prices[i]>prices[i-1]:
+                profit+=prices[i]-prices[i-1]
         return profit
         
--- src/26-remove-duplicates-from-sorted-array.py
+++ src/26-remove-duplicates-from-sorted-array.py
@@ -1,11 +1,24 @@
 '''https://leetcode.com/problems/remove-duplicates-from-sorted-array/'''
 
+# class Solution:
+#     def removeDuplicates(self, nums: List[int]) -> int:
+#         l = len(nums)
+#         ans = 0
+#         for i in range(1, l):
+#             if nums[i]!=nums[ans]:
+#                 ans+=1
+#                 nums[i], nums[ans] = nums[ans], nums[i]
+#         return ans+1
+
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        l = len(nums)
-        ans = 0
-        for i in range(1, l):
-            if nums[i]!=nums[ans]:
-                ans+=1
-                nums[i], nums[ans] = nums[ans], nums[i]
-        return ans+1
+        if len(nums)<2:
+            return len(nums)
+        p1, p2 = 0, 1 
+        while p2<len(nums):
+            if nums[p2]!=nums[p1]:
+                p1+=1
+                nums[p1] = nums[p2]
+            p2+=1
+        return p1+1
+        
